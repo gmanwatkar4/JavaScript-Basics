e5c4615063da9e5696db41dfbdb0bec03060ba51
gmanwatkar4 <39916428+gmanwatkar4@users.noreply.github.com>
2024-03-19 10:18:35 +0000

    data types

---
+++ .devcontainer/01_Basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict" // treat all JS code as per the newer version
+
+// alert("hello") // we are using node.js, not a browser
+
+let name = "Faizal"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+// Data types
+// number -: 2 to the power 53
+// bigInt
+// string -: ""
+// boolean -: true/false
+// null -: standalone value
+// undefined -: value is not defined
+// symbol -: unique
+
+console.log(typeof "Faizal") // type is string
+console.log(typeof(null)) // type is object
+console.log(typeof(undefined)) // type is undefined
+
+
