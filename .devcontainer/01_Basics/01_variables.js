9d4a47405a564197e4ce02ff15d9c53223beedf8
gmanwatkar4 <39916428+gmanwatkar4@users.noreply.github.com>
2024-03-19 08:16:57 +0000

    we learned the vriables in javascript

---
+++ .devcontainer/01_Basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "javascript@gmail.com"
+var accountPassword = "12345"
+accountCity = "Nagpur"
+let accountState
+
+// accountId = 2 // not allowed
+
+accountEmail = "js@gmail.com"
+accountPassword = "21011996"
+accountCity = "Pune"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+// we can use let to declare the variables
+// prefer not to use var because of the block scope and functional scope
+//If we did not assigned any value to the variable in the javascript, the default value is undefined
