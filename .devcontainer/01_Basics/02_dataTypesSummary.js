2e57a9fc1391a5ae2c46e7524cb20185ffe4c3e4
gmanwatkar4 <39916428+gmanwatkar4@users.noreply.github.com>
2024-03-20 06:53:29 +0000

    memory types - stack and heap

--- .devcontainer/01_Basics/02_dataTypesSummary.js
+++ .devcontainer/01_Basics/02_dataTypesSummary.js
@@ -1,9 +1,9 @@
 // Primitive data types
 // 7 types : String, Number, Boolean, Null, Undefined, BigInt, Symbol
 
-// Examples of primitive datat types
+// Examples of primitive data types
 
-const score = 100
+const score = 100 
 const scoreValue = 100.3
 const isLoggedIn = false
 const outsideTemp = null
@@ -35,3 +35,38 @@ console.log(typeof scoreValue); // return type of scoreValue is number
 console.log(typeof heros); // return type of heros is object
 console.log(typeof myObj); // return type of myObj is object
 console.log(typeof myFunction); // return type of myFunction is function also called object function
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Types of memories - Stack, Heap
+// Stack memory is used in primitive data types
+// Heap memory is used in non primitive data types
+
+// When the stack memory is used, you will get a copy of the declared variable
+// when the heap memory is used, you will get a reference of the declared variable or original value 
+
+// Examples of non primitive data type
+// Examples of Stack
+let myYoutubeName = "javascript.com"
+
+let anotherName = myYoutubeName // you will get a copy of myYoutubeName
+anotherName = "myjavascript.com"
+
+console.log(myYoutubeName); // prints javascript.com 
+console.log(anotherName); // prints myjavascript.com
+
+// Examples of Heap
+let userOne = {
+    email : "user@google.com",
+    upiID : "123@ybl" 
+}
+
+let userTwo = userOne // you will get a reference of userOne 
+
+userTwo.email = "faizal@yahoo.com"
+
+console.log(userOne.email); // returns faizal@yahoo.com
+console.log(userTwo.email); // returns faizal@yahoo.com
+
+// In stack changes will be done in the copy of a variable, original value remains same
+// In heap changes will be done in the original value
