8bf31558d158bddd36ee162bba68d7e95d8313ec
gmanwatkar4 <39916428+gmanwatkar4@users.noreply.github.com>
2024-03-22 14:20:08 +0000

    functions

---
+++ .devcontainer/03_Basics/01_functions.js
@@ -0,0 +1,12 @@
+// Simple function
+
+function sayMyName (){
+    console.log("F");
+    console.log("a");
+    console.log("i");
+    console.log("z");
+    console.log("a");
+    console.log("l");
+}
+
+console.log(sayMyName());
