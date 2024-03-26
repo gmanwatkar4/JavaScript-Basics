c0943f32d86131f8faceb5d3fea5e373fbf3850a
gmanwatkar4 <39916428+gmanwatkar4@users.noreply.github.com>
2024-03-26 08:36:26 +0000

    intro to functions

--- .devcontainer/03_Basics/01_functions.js
+++ .devcontainer/03_Basics/01_functions.js
@@ -1,6 +1,6 @@
 // Simple function
 
-function sayMyName (){
+function sayMyName(){
     console.log("F");
     console.log("a");
     console.log("i");
@@ -9,4 +9,38 @@ function sayMyName (){
     console.log("l");
 }
 
-console.log(sayMyName());
+sayMyName(); // prints each letter of Faizal on the new line
+
+// function to add 2 numbers
+
+function addTwoNumbers(num1, num2){ // here num1 and num2 are parameters
+    console.log(num1 + num2)
+}
+addTwoNumbers(5, 5);  // prints 10 -> here 5, 5 are arguments
+
+const result = addTwoNumbers(5, 6) // prints 11
+
+console.log("Result : ", result) // prints the result value as undefined cause the value was not returned by addTwoNumbers function
+
+// returning the value by the function
+
+function addThreeNumbers(num1, num2, num3){
+    const result = num1 + num2 + num3
+    return result
+}
+const result1 = addThreeNumbers(5, 5, 5)
+console.log(result1);
+
+// print the parameter
+
+function loginUserMessage(username){
+    return `Hi, ${username} you just logged in!`
+}
+console.log(loginUserMessage("Faizal")); // prints Hi, Faizal you just logged in!
+
+// when you doesn't pass any argument to the function it returns undefined
+
+function sheLovesMe(answer){
+    return `She loves me ? ${answer}`
+}
+console.log(sheLovesMe()); // prints She loves me ? undefined
